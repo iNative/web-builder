086e936af26414e4657bac5341391b63daf47dd9
guido serio <gserio@olon.it>
2019-03-30 19:55:29 +0100

    azienda form

--- dist/assets/js/material-kit.js
+++ dist/assets/js/material-kit.js
@@ -14,6 +14,46 @@
 var big_image;
 
 $(document).ready(function() {
+    $(function(){
+  
+        var $cat = $("#category1"),
+            $subcat = $(".subcat");
+  
+        var optgroups = {};
+  
+        $subcat.each(function(i,v){
+        	var $e = $(v);
+        	var _id = $e.attr("id");
+    			optgroups[_id] = {};
+    			$e.find("optgroup").each(function(){
+          	var _r = $(this).data("rel");
+            $(this).find("option").addClass("is-dyn");
+          	optgroups[_id][_r] = $(this).html();
+    			});
+        });
+        $subcat.find("optgroup").remove();
+  
+        var _lastRel;
+        $cat.on("change",function(){
+            var _rel = $(this).val();
+            if(_lastRel === _rel) return true;
+            _lastRel = _rel;
+            $subcat.find("option").attr("style","");
+            $subcat.val("");
+            $subcat.find(".is-dyn").remove();
+            if(!_rel) return $subcat.prop("disabled",true);
+            $subcat.each(function(){
+            	var $el = $(this);
+              var _id = $el.attr("id");
+              $el.append(optgroups[_id][_rel]);
+            });
+            $subcat.prop("disabled",false);
+        });
+  
+    });
+	
+	
+	
     BrowserDetect.init();
 
     // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
